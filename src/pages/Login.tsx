005a437d03b7f6b25fac45b85459cf2b7c7f4d12
[dyad] <johnsonchelestine27@gmail.com>
2025-09-13 09:44:18 +0300

    [dyad] Fixing syntax error in Login.tsx - wrote 1 file(s)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,7 @@
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import *s z from "zod";
+import * as z from "zod";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import {
